13d440fdba82aa3871ae357069baf683c335d18f
Jeniffer Carvalho <jenicarvalho@live.com>
2019-12-08 13:14:09 -0300

    adicionado Provider no app e criado a store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import Cart from "./Cart";
+import cartReducer from "./reducers";
+
+const store = createStore(cartReducer);
+
+store.dispatch({ type: "ADD_TO_CART", payload: { id: 1, item: "iphone x" } });
+store.dispatch({ type: "GET_ITEMS_CART" });
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Cart />
+    </Provider>
   );
 }
 
